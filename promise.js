501f99f97f198dcf035000c8ac7d7e589dd9fc78
三重_刘德华 <moonthief@foxmail.com>
2024-12-10 16:20:02 +0800

    微队列实现

--- promise.js
+++ promise.js
@@ -3,12 +3,49 @@ const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
 
 class MyPromise {
-    /** 私有属性和私有方法 */
-    #state = PENDING//#私有属性，等同于在构造器里写this._state = 'pending'
+
+    /* 构造器 */
+    constructor(executor) {
+        const resolve = (data) => {
+            this.#changeState(FULFILLED, data)
+        }
+        const reject = (reason) => {
+            this.#changeState(REJECTED, reason)
+        }
+        //异常放在try-catch里面捕获，就直接用reject处理他
+        try {
+            executor(resolve, reject)//只能捕获同步任务的错误，异步任务的错误捕获不到（官方问题）
+        }
+        catch (err) {
+            reject(err)
+        }
+    }
+
+    /* then有两个任务，添加数组，执行run */
+    then(onFulfilled, onRejected) {
+        return new MyPromise((resolve, reject) => {
+            this.#handlers.push({
+                onFulfilled,
+                onRejected,
+                resolve,
+                reject
+            })
+            this.#run()
+        })
+    }
+
+    /* 私有属性和私有方法 */
+    #state = PENDING//#表示为私有属性，等同于在构造器里写this._state = 'pending'
     #result = undefined
     #handlers = []//用来存储 onFulfilled, onRejected, resolve, reject，为啥是数组呢，因为用户可能多次调用then
 
-    #changeState(state, result) {//#私有方法
+    /**
+     * @function #changeState 
+     * @description 改变状态的方法
+     * @param {string} state state为目标状态
+     * @param {*} result result为目标数据
+     */
+    #changeState(state, result) {//#表示为私有方法
         if (this.#state !== PENDING) return //防止promise执行两次
         this.#state = state
         this.#result = result
@@ -28,8 +65,30 @@ class MyPromise {
         return false
     }
 
+    /**
+     * @function #runMicroTask
+     * @description 执行微队列
+     * @param {function} fn 
+     * @returns {boolean} 
+     */
     #runMicroTask(fn) {
-        setTimeout(fn, 0)
+        //用process.nextTick模拟node环境微队列
+        if(typeof process === 'object' && typeof process.next === 'function'){
+            process.nextTick(fn)
+        }
+        //用MutationObserver模拟浏览器环境微队列
+        else if(typeof MutationObserver === 'function'){
+            const ob = new MutationObserver(fn)
+            const textNode = document.createTextNode('1')
+            ob.observe(textNode,{
+                characterData:true
+            })
+            textNode.data = '2'
+        }
+        //除上面两种环境，只能用setTimeout了，因为微队列是环境给你的能力(node的事件循环,浏览器的事件循环)
+        else{
+            setTimeout(fn,0)
+        }
     }
 
     #runone(callback, resolve, reject) {
@@ -68,56 +127,23 @@ class MyPromise {
             }
         }
     }
-
-    //then有两个任务，添加数组，执行run
-    then(onFulfilled, onRejected) {
-        return new MyPromise((resolve, reject) => {
-            this.#handlers.push({
-                onFulfilled,
-                onRejected,
-                resolve,
-                reject
-            })
-            this.#run()
-        })
-
-    }
-
-    /**构造器 */
-    constructor(executor) {
-        const resolve = (data) => {
-            this.#changeState(FULFILLED, data)
-        }
-        const reject = (reason) => {
-            this.#changeState(REJECTED, reason)
-        }
-        //异常放在try-catch里面捕获，就直接用reject处理他
-        try {
-            executor(resolve, reject)//只能捕获同步任务的错误，异步任务的错误捕获不到（官方问题）
-        }
-        catch (err) {
-            reject(err)
-        }
-    }
 }
 
-const p = new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-        reject(123)
-    }, 1000)
+/* 测试区域 */
+
+setTimeout(()=>{
+    console.log(1)
+},0)
+
+new MyPromise((resolve)=>{
+    resolve(2)
+}).then((data)=>{
+    console.log(data)
 })
 
-p.then(
-    null,
-    (err) => {
-        console.log('promise失败1', err)
-        return 456
-    }
-).then((data) => {
-    console.log('ok', data)
-})
+console.log(3)
 
-
+// FIXME: 在浏览器测试为321，在vscode测试为312
 
 /**
  * 
